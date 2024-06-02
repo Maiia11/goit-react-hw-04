47b42fc9bc66b5952daffff10b711d2d471da878
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-06-02 14:34:30 +0300

    add components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+
+// import { useEffect } from 'react';
+import './App.css'
+// import axios from 'axios';
+
+// const key = 'zDei0CTQ82bIpGgBAWl-KIKshW8lXqg3k_pY_IM5bRY';
+
+
+
+function App() {
+
+  // useEffect(() => {
+  //   async function fetch() {
+  //     const response = await axios.get('https://api.unsplash.com/search/photos?client_id=zDei0CTQ82bIpGgBAWl-KIKshW8lXqg3k_pY_IM5bRY&page=1&query=office');
+  //     console.log(response)
+      
+  //   }
+  //   fetch()
+  // }, [])
+  
+}
+
+export default App
